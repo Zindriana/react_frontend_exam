975e8fc5b989b51cc734156da6c632bb64fb321f
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-11-07 15:55:29 +0100

    updated the imports and names in the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { HashRouter, Route, Routes, Link } from 'react-router-dom';
-import CharacterList from "./components/HeroList.jsx";
-import NewCharacter from "./components/NewHero.jsx";
 import ScrollList from "./components/ScrollList.jsx";
 import NewScroll from "./components/NewScroll.jsx";
+import HeroList from "./components/HeroList.jsx";
+import NewHero from "./components/NewHero.jsx";
 
 function App() {
 
@@ -12,14 +12,14 @@ function App() {
                 <nav>
                     <Link to="/"> Home </Link>
                     <Link to="/herolist" className="heroListLink"> || Character List </Link>
-                    <Link to="/newhero" className="newheroLink"> || New Character </Link>
+                    <Link to="/newhero" className="newHeroLink"> || New Character </Link>
                     <Link to="/scrolllist" className="scrollListLink"> || Scroll List </Link>
-                    <Link to="/newscroll" className="newscrollLink"> || New Scroll </Link>
+                    <Link to="/newscroll" className="newScrollLink"> || New Scroll </Link>
                 </nav>
                 <Routes>
                     <Route path="/" element={<div>Home</div>}/>
-                    <Route path="/characterlist" element={<CharacterList/>}/>
-                    <Route path="/newcharacter" element={<NewCharacter/>}/>
+                    <Route path="/characterlist" element={<HeroList/>}/>
+                    <Route path="/newcharacter" element={<NewHero/>}/>
                     <Route path="/scrolllist" element={<ScrollList/>}/>
                     <Route path="/newscroll" element={<NewScroll/>}/>
                 </Routes>
